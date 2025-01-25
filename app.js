8dad77f083d3a8ede58ac9c05e571ed35ab8230d
agustin <agustinalvarez7378@gmail.com>
2025-01-25 11:31:00 -0400

    implementando una función para agregar amigo y otra para limpiar el input

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+let seleccionar = document.getElementById('amigo');
+
+function agregarAmigo () {
+   if(amigos == "") {
+    alert("el campo está vacio, ingresa un valor valido")
+   } else {
+    agregandoAmigos = amigos.push('#amigo')
+   }
+}
+agregarAmigo();
+
+function limpiarCaja () {
+    let valorCaja = document.querySelector('#amigo').value = '';
+    return;
+}
+limpiarCaja();
